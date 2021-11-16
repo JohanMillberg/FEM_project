d54696300ddfc71bb9379cb42b88599699221d8f
Karl Pontén <karlponten@hotmail.com>
2021-11-16 19:48:27 +0100

    verymanychanges

--- levmarq.m
+++ levmarq.m
@@ -4,15 +4,28 @@ function [x, resnorm, residual] = levmarq(func, x0)
 
 x = x0; 
 r = 100;
-delta_k = 0.01;
+delta_k = 10;
 lambda = 1/4;
 mu = 1/4;
 eta = 3/4;
 
-while norm(r) < 0.001
-    [r, J] = func(x);
-    p = inv(J'*J+lambda*eye(length(x)))*J'*r;
-    x = x + p';
+for i = 1:5
+    [f, r, J] = func(x);
+    lambdafun = @(l)norm((J'*J+l*eye(length(x)))\J'*r)-delta_k;
+    lambda = fzero(lambdafun, 1);
+    p = (J'*J+lambda*eye(length(x)))\J'*r;
+    
+    q = norm(f + J*p);
+    [f_new, r_new, J_new] = func(x+p);
+    rho = norm(f - f_new)/norm(-J*p);
+    
+    if rho <= mu
+        delta_k = 1/2*delta_k
+    elseif rho < eta
+        x = x + p
+    else
+        delta_k = 2*delta_k
+        x = x + p
 end
 
 [r, J] = func(x);
--- residualfunc.m
+++ residualfunc.m
@@ -1,13 +1,14 @@
-function [r,rgrad] = residualfunc(x)
+function [fun,r,rgrad] = residualfunc(x)
 %RESIDUALFUNC Summary of this function goes here
 %   Detailed explanation goes here
 
-f = @(x, t) x(1)*exp(x(2)*t)
+f = @(x, t) x(1)*exp(x(2)*t);
 
 t = [0.5, 1, 1.5, 2, 2.5, 3.0, 3.5, 4.0]';
 y = [6.8, 3.0, 1.5, 0.75, 0.48, 0.25, 0.2, 0.15]';
+fun = f(x, t)
 r = (y-f(x, t));
-rgrad = jacobian_fd(f, x, t, 0.025);
-%rgrad = [exp(x(2)*t), (t).*x(1).*exp(x(2)*t)];
+%rgrad = jacobian_fd(f, x, t, 0.025);
+rgrad = [exp(x(2)*t), (t).*x(1).*exp(x(2)*t)];
 end
 
