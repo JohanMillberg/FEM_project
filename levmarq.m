ce2a6ed48ef4133848bede4943f9ce1961ab9167
Karl Pontén <karlponten@hotmail.com>
2021-11-18 19:34:47 +0100

    hehe

--- levmarq.m
+++ levmarq.m
@@ -2,30 +2,37 @@ function [x, resnorm, residual] = levmarq(func, x0)
 %LEVMARQ Summary of this function goes here
 %   Detailed explanation goes here
 
-x = x0; 
+x = x0;
 r = 100;
-delta_k = 10;
-lambda = 1/4;
+delta_k = 1;
+lambda = 0;
 mu = 1/4;
 eta = 3/4;
 
-for i = 1:5
-    [f, r, J] = func(x);
-    lambdafun = @(l)norm((J'*J+l*eye(length(x)))\J'*r)-delta_k;
-    lambda = fzero(lambdafun, 1);
-    p = (J'*J+lambda*eye(length(x)))\J'*r;
-    
+while norm(r) > 0.35
+    [r, J] = func(x);
+    lambda = 0;
+    p = (J'*J+lambda*eye(length(x)))\(J'*r);
+    disp(inv(J'*J+lambda*eye(length(x))))
+    if norm(p) > delta_k
+        lambdafun = @(l)(norm((J'*J+l*eye(length(x)))\(J'*r))-delta_k);
+        lambda = fzero(lambdafun, 100);
+        p = (J'*J+lambda*eye(length(x)))\J'*r;
+    end
+    disp(lambda)
+        
     q = norm(f + J*p);
-    [f_new, r_new, J_new] = func(x+p);
-    rho = norm(f - f_new)/norm(-J*p);
+    [r_new, J_new] = func(x+p);
+    rho = norm(r - r_new)/norm(-J*p);
     
     if rho <= mu
-        delta_k = 1/2*delta_k
+        delta_k = 1/2*delta_k;
     elseif rho < eta
-        x = x + p
+        x = x + p;
     else
-        delta_k = 2*delta_k
-        x = x + p
+        delta_k = 2*delta_k;
+        x = x + p;
+    end
 end
 
 [r, J] = func(x);
---
+++ levmarqm.m
@@ -0,0 +1,38 @@
+function [x, resnorm, residual] = levmarqm(func, x0)
+%LEVMARQ Summary of this function goes here
+%   Detailed explanation goes here
+
+x = x0;
+r = 100;
+lambda = 10;
+nu = 1.5;
+
+while norm(r) > 0.4
+    [r, J] = func(x);
+    disp(J'*J)
+    p1 = (J'*J+lambda*eye(length(x)))\(J'*r);
+    p2 = (J'*J+lambda/nu*eye(length(x)))\(J'*r);
+    [r1, J1] = func(x+p1);
+    [r2, J2] = func(x+p2);
+    
+    if norm(r1) < norm(r2)
+        if norm(r1) < norm(r)
+            x = x + p1;
+        else
+            lambda = lambda*nu;
+        end
+    else
+        if norm(r2) < norm(r)
+            x = x + p2;
+            lambda = lambda/nu;
+        else
+            lambda = lambda*nu;
+        end
+    end
+    disp(lambda)
+end
+
+[r, J] = func(x);
+resnorm = norm(r);
+residual = r;
+end
