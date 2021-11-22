29d0c83837b52e3f0df643c6b99ae72098034bf4
Kvarnberg6 <johanmillberg@me.com>
2021-11-22 11:15:22 +0100

    nej

--- levmarqm.m
+++ levmarqm.m
@@ -1,15 +1,18 @@
-function [x, resnorm, residual] = levmarqm(func, x0)
+function [x, resnorm, residual, lambda_vec, iter] = levmarqm(func, x0)
 %Function used to solve non linear least square curve fitting problems
 %func: The residual function used to calculate the residuals, gradient of
 %the residuals, and the function f
 %x0: the starting guess
 x = x0;
 r = 100;
-lambda = 10;
+lambda_vec = [];
+lambda = 1;
 nu = 1.5;
+iii = 0;
+prev_r = 0;
 
-
-while norm(r) > 0.4
+while abs((norm(r) - norm(prev_r))/norm(r)) > 1e-6
+    iii = iii + 1;
     [r, J] = func(x);
     %Take two LM steps, one with damping lambda and one with lambda/nu
     p1 = (J'*J+lambda*eye(length(x)))\(J'*r);
@@ -19,21 +22,26 @@ while norm(r) > 0.4
     
     if norm(r1) < norm(r2)
         if norm(r1) < norm(r)
+            prev_r = r;
+            r = r1;
             x = x + p1;
         else
             lambda = lambda*nu;
         end
     else
         if norm(r2) < norm(r)
+            prev_r = r;
+            r = r1;
             x = x + p2;
             lambda = lambda/nu;
         else
             lambda = lambda*nu;
         end
+    lambda_vec(iii) = lambda;
     end
 end
-
 [r, J] = func(x);
+iter = linspace(1,iii,iii);
 resnorm = norm(r);
 residual = r;
 end
--- ttt.m
+++ ttt.m
@@ -3,15 +3,37 @@
 fun = @(b)residualfunc(b);
 x0 = [1;1];
 
-[x_sol, rn, r] = levmarqm(fun, x0);
+options = optimset();
 
+[x_sol, rn, r, lambda, iter] = levmarqm(fun, x0);
+[x_sol_mat, rn_mat] = lsqnonlin(fun, x0);
 
 t = [0.5, 1, 1.5, 2, 2.5, 3.0, 3.5, 4.0]';
 y = [6.8, 3.0, 1.5, 0.75, 0.48, 0.25, 0.2, 0.15]';
-[a,b,approx_y] = fun(x_sol);
 
+[a,b,approx_y] = fun(x_sol);
+[a,b,approx_y_mat] = fun(x_sol_mat);
+
+disp(lambda(end))
+figure()
 hold on
 plot(t,y,'x',t, approx_y(x_sol, t))
 xlabel('t');
 ylabel('y');
-legend(['Given points', 'Approximated solution '+string(round(x_sol(1),2))+'*exp('+string(round(x_sol(2),2))+'*t)']);
+legend(['Data points', 'Approximated solution '+string(round(x_sol(1),2))+'*exp('+string(round(x_sol(2),2))+'*t)']);
+hold off
+
+figure()
+disp(lambda(end))
+hold on
+plot(t, approx_y_mat(x_sol_mat, t), 'x-', t, approx_y(x_sol, t))
+xlabel('t');
+ylabel('y');
+legend(['Lsqnonlin', 'Approximated solution '+string(round(x_sol(1),2))+'*exp('+string(round(x_sol(2),2))+'*t)']);
+hold off
+
+figure()
+hold on
+plot(iter, lambda)
+xlabel('iteration');
+ylabel('Damping factor \lambda')
